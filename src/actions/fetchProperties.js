09fddaf12d81485e8b6541220829c462e204ce09
steelerx155 <steelerx155@gmail.com>
2021-02-17 11:46:03 -0500

    added actions and components folder and created PropertiesList.js component

---
+++ src/actions/fetchProperties.js
@@ -0,0 +1,15 @@
+export function fetchProperties() {
+fetch("https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=1178275040", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "7b02074c19mshedbbe70e8fd2ae2p163b20jsne2411a44c687",
+		"x-rapidapi-host": "hotels4.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+}
